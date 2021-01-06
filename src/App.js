a4c0b18a346a6dab997051468a6fec296963c2c0
Divyanshi Agrawal <divsagr3@gmail.com>
2021-01-06 20:44:31 +0530

    Add recipe modal

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Search from "./Components/Search";
 import Cards from "./Components/Cards";
+import Recipe from "./Components/Recipe";
+
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Search />
+      <Recipe />
       <Cards />
     </div>
   );
